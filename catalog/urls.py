a69d8795b0b5aec865635f6aa159179817911020
Evgeniy <stssokolsts@yandex.ru>
2014-10-19 19:55:11 +0400

    yeeeeeeee1

---
+++ catalog/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import *
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = patterns('catalog.views',
+                       url(r'^$', 'index', { 'template_name' :'catalog/index.html'}, 'catalog_home'),
+                       url(r'^category/(?P<category_slug>[-\w]+)/$', 'show_category', {'template_name':'catalog/category.html'},
+                           'catalog_category'),
+                       url(r'^product/(?P<product_slug>[-\w]+)/$', 'show_product', {'template_name':'catalog/product.html'},
+                           'catalog_product'),
+                       url(r'^catalog$','show_catalog',{'template_name':'catalog/catalog.html' },'catalog'),
+                       url(r'^akcii/', 'show_discounts', {'template_name':'catalog/discounts.html'},
+                           'discounts'),
+                       )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
